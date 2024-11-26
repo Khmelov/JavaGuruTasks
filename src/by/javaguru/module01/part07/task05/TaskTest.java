a7b42a635e44ab015b8fc6d770950055283905b5
akhmelev <akhmelev@gmail.com>
2024-11-26 20:00:07 +0300

    Fix 1.7.5

--- src/by/javaguru/module01/part07/task05/TaskTest.java
+++ src/by/javaguru/module01/part07/task05/TaskTest.java
@@ -5,8 +5,8 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 @SuppressWarnings("all")
@@ -14,22 +14,22 @@ public class TaskTest extends HomeWork {
 
     @Test(timeout = 2000)
     public void testSolution() throws Exception {
-        String out = run("10").strOut.toString();
-        String[] strings = out
+        var out = run("10").strOut.toString();
+        var strings = out
                 .replace("\n", "")
                 .replaceAll("].*", "")
                 .replaceAll(".*\\[", "")
                 .split("[, ]+");
-        List<Integer> list = Arrays.stream(strings).map(Integer::valueOf).toList();
-        list = new ArrayList<>(list);
+        assertEquals("Incorrect size", 10, strings.length);
+        var list = new ArrayList<>(
+                Arrays.stream(strings).map(Integer::valueOf).toList()
+        );
         for (int l = 0, r = 0; r < list.size(); r++) {
             int n = list.size() - 1;
-            if (r != l && list.get(l) != list.get(r)) {
-                l = r;
-            }
+            l = (r != l && list.get(l) != list.get(r)) ? r : l;
             if (
                     (l == 0 && r < n && list.get(r) > list.get(r + 1)) ||
-                    (r == n && list.get(l) > list.get(l - 1)) ||
+                    (r == n && l > 0 && list.get(l) > list.get(l - 1)) ||
                     (l > 0 && r < n &&
                      list.get(r) > list.get(r + 1) &&
                      list.get(l) > list.get(l - 1))
